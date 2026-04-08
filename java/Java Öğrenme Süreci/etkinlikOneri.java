7ccf0c5c3e7a3a66df619ed6b0f52975fdf95402
MusaAlver <musa.alverr@gmail.com>
2026-04-08 19:26:27 +0300

    Rename AlverLabs class to etkinlikOneri

--- java/Java Öğrenme Süreci/etkinlikOneri.java
+++ java/Java Öğrenme Süreci/etkinlikOneri.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-public class AlverLabs {
+public class etkinlikOneri {
     public static void main(String[] args) {
         Scanner girdi = new Scanner(System.in);
 
